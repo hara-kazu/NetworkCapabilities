d05b9412725aaaa8bed062ed0bb8b2714cddb0e0
hara-kazu <harakazu103@gmail.com>
2022-05-04 01:51:54 +0900

    初期コミット

---
+++ NetworkCapabilities.cs
@@ -0,0 +1,61 @@
+﻿namespace NetworkUtils
+{
+    using System.Net.NetworkInformation;
+	using System.Management;
+	using System.Text.RegularExpressions;
+	using System.Runtime.Versioning;
+
+	/// <summary>
+	/// ネットワークの有効・無効切り替え機能を提供する
+	/// </summary>
+	/// <remarks>
+	/// このライブラリは、Windows のみサポートしている
+	/// </remarks>
+	[SupportedOSPlatform("Windows")]
+	public class NetworkCapabilities
+    {
+		/// <summary>
+		/// 正規表現での検証に使用する イーサーネットを表す識別子
+		/// </summary>
+		const string RegixPatternIndex = "[イーサネット]";
+
+		/// <summary>
+		/// ネットワークを有効化する
+		/// </summary>
+		public void EnableNetwork()
+			=> IssueNetworkQuery(true);
+
+		/// <summary>
+		/// ネットワークを無効化する
+		/// </summary>
+		public void DisableNetwork()
+			=> IssueNetworkQuery(false);
+
+		/// <summary>
+		/// ネットワークが有効化どうかを示す値を取得
+		/// </summary>
+		/// <returns>true:ネットワークは有効.false:ネットワークは無効</returns>
+		public bool IsEnableNetwork()
+			=> NetworkInterface.GetIsNetworkAvailable();
+
+		/// <summary>
+		/// ネットワークの有効・無効のクエリを発行
+		/// </summary>
+		/// <param name="enable">true:ネットワークを有効.false:ネットワークを無効</param>
+		void IssueNetworkQuery(bool enable)
+		{
+			// NICの検索
+			var wmiQuery = new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionId != NULL");
+			using (var searcher = new ManagementObjectSearcher(wmiQuery)) {
+				foreach (var item in searcher.Get().OfType<ManagementObject>()) {
+					if (!Regex.IsMatch((string)item["NetConnectionId"], RegixPatternIndex))
+						continue;
+
+					using (item) {
+						item.InvokeMethod(enable ? "Enable" : "Disable", null);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ NetworkInfomation.cs
@@ -0,0 +1,50 @@
+﻿
+namespace NetworkUtils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.NetworkInformation;
+
+    /// <summary>
+    /// ネットワークに関する情報を提供する
+    /// </summary>
+    public static class NetworkInfomation
+    {
+        /// <summary>
+        /// ネットワークに接続中の一番最初に見つかった NIC の情報を取得
+        /// </summary>
+        /// <returns></returns>
+        public static NetworkInterface? GetNetworkInterfaceFirst()
+            => NetworkInterface.GetAllNetworkInterfaces().Where(x => IsConnectNetwork(x)).FirstOrDefault();
+
+        /// <summary>
+        /// 無線接続かどうか
+        /// </summary>
+        /// <returns>true:無線接続.false:有線接続</returns>
+        public static bool IsWired()
+            => NetworkInterface.GetAllNetworkInterfaces().Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Wireless80211).Any();
+
+        /// <summary>
+        /// 有線接続かどうか
+        /// </summary>
+        /// <returns>true:有線接続.false:無線接続</returns>
+        public static bool IsNonWired()
+            => NetworkInterface.GetAllNetworkInterfaces().Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Ethernet).Any();
+
+        /// <summary>
+        /// 指定のネットワークインターフェースがネットワークに接続中かどうかを示す値を取得
+        /// </summary>
+        /// <param name="netInterface">NetworkInterface</param>
+        /// <remarks>
+        /// ネットワーク接続中かどうか以下条件で、判定する
+        /// 1. ネットワークインターフェースが稼働している (OperationalStatus.Up)
+        /// 2. ネットワークインターフェースの種類がトンネル接続でもループバック (NetworkInterfaceType.Tunnel かつ Loopback) でない
+        /// </remarks>
+        /// <returns>true:ネットワークに接続中.false:ネットワークに未接続</returns>
+        static bool IsConnectNetwork(NetworkInterface netInterface)
+            => netInterface.OperationalStatus == OperationalStatus.Up
+            && !(netInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback || netInterface.NetworkInterfaceType == NetworkInterfaceType.Tunnel);
+
+    }
+}
